93c40e592aeefa3c4849f503cf10ead211d38769
Elias Ylänen <elias.ylanen@gmail.com>
2017-04-24 15:58:53 +0300

    Sails routes testing

--- server/api/controllers/FooController.js
+++ server/api/controllers/FooController.js
@@ -2,4 +2,12 @@ module.exports = {
   bar: (req, res) => {
     res.json({ message: 'hello' });
   },
+  foobar: (req, res) => {
+    console.log(req.body);
+    res.ok();
+  },
+  editBoard: (req, res) => {
+    console.log(req.param('boardId'));
+    res.ok();
+  },
 };
